8150468aefcfaaa3b9c26b83a63de9ca5a1fb5b0
SimoVisc <simonaviscardivs@gmail.com>
2022-11-25 15:39:50 +0100

    installa il pacchetto vite attraverso npm e aggiunge le immagini

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+
+import AppComics from './components/AppComics.vue'
+
+export default {
+  components: {
+    AppComics
+  }
+}
+</script>
+
+<template>
+  <div>
+    <AppComics msg=""/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
