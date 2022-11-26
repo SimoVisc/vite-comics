52c7566d0166c5ee419c393b1a74efb85d9db8ac
SimoVisc <simonaviscardivs@gmail.com>
2022-11-26 10:30:22 +0100

    Aggiunge appLogo e immagine logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,17 @@
 <script>
+    import AppLogo from './AppLogo.vue';
+
     export default {
-    name: "AppHeader",
-   }
+        name: "AppHeader",
+        components:{
+            AppLogo
+        }
+    }
 </script>
 
 <template>
   <h1></h1>
+  <AppLogo/>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppLogo.vue
@@ -0,0 +1,12 @@
+<script>
+    export default {
+    name: "AppLogo",
+   }
+</script>
+
+<template>
+  <img src="../assets/img/dc-logo.png" alt="logo">
+</template>
+
+<style lang="scss">
+</style>
